6d444127ea2d069645d8e98fa072c2e95cb8a4b1
Josef <wengler@student.tugraz.at>
2022-07-17 20:09:34 +0200

    Initial Commit

---
+++ Due/DueApp.swift
@@ -0,0 +1,17 @@
+//
+//  DueApp.swift
+//  Due
+//
+//  Created by Josef Wengler on 17.07.22.
+//
+
+import SwiftUI
+
+@main
+struct DueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
