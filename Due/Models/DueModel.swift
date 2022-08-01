614e9871ee7f1d294a1a4c262505e41ba7717f24
Tanja <kobler.tanja@gmail.com>
2022-08-01 16:00:01 +0200

    better overview

--- Due/Models/DueModel.swift
+++ Due/Models/DueModel.swift
@@ -56,16 +56,26 @@ extension DueModel {
 extension DueModel {
     static let sampleData: [DueModel] =
     [
-        DueModel(title: "3D Projekt", attendees: ["Tanja", "Josef", "Sarah", "Samuel"],
+        DueModel(
+                title: "3D Projekt",
+                attendees: ["Tanja", "Josef", "Sarah", "Samuel"],
                 tasks: [TaskModel(title: "3D Design Task", task_time: "18:00", task_date: "29.03"),
-                        TaskModel(title: "3D Papers", task_date: "16.04")], due_date: "20.04", theme: .yellow),
+                        TaskModel(title: "3D Papers", task_date: "16.04")],
+                due_date: "20.04",
+                theme: .yellow),
         
-        DueModel(title: "Operating Systems", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+        DueModel(
+                title: "Operating Systems",
+                attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
                 tasks: [TaskModel(title: "Write in C", task_date: "03.04"),
                         TaskModel(title: "Love SEG-Fault",  task_date: "22.04"),
-                        TaskModel(title: "TeamWork",  task_date: "02.05")], due_date: "21.05", theme: .sky),
+                        TaskModel(title: "TeamWork",  task_date: "02.05")],
+                due_date: "21.05",
+                theme: .sky),
         
         DueModel(title: "OOP 2", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla"],
-                 tasks: [TaskModel(title: "Write in JAVA", task_date: "06.04")], due_date: "12.04", theme: .poppy)
+                 tasks: [TaskModel(title: "Write in JAVA", task_date: "06.04")],
+                 due_date: "12.04",
+                 theme: .poppy)
     ]
 }
