95fec6c743bac7acac3011970e75b4ab809f0f54
tanjawebdev <kobler.tanja@gmail.com>
2022-08-02 19:38:22 +0200

    Add Colors and Fonts

--- Due/Models/DueModel.swift
+++ Due/Models/DueModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct DueModel: Identifiable {
     let id: UUID
@@ -71,11 +72,59 @@ extension DueModel {
                         TaskModel(title: "Love SEG-Fault",  task_date: "22.04"),
                         TaskModel(title: "TeamWork",  task_date: "02.05")],
                 due_date: "21.05",
-                theme: .sky),
+                theme: .blue),
         
         DueModel(title: "OOP 2", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla"],
                  tasks: [TaskModel(title: "Write in JAVA", task_date: "06.04")],
                  due_date: "12.04",
-                 theme: .poppy)
+                 theme: .red)
     ]
 }
+
+
+
+// JOSEF PLEASE CHECK DU MAL DES MIT DE FONTS AUS, IDGI
+
+// Links mit erklärungen:
+// https://www.youtube.com/watch?v=l5QOsnNpiOw&ab_channel=Let%27sFinishthisAPP-KotlinandSwiftTutorials
+// https://www.youtube.com/watch?v=AL_2ZgtF4Vk&ab_channel=KtrKathir
+// https://sarunw.com/posts/scaling-custom-fonts-automatically-with-dynamic-type/
+
+// des geht:
+//.font(.custom("Poppins-SemiBold", size: 12))
+
+// des geht ned:
+
+//let customFonts: [UIFont.TextStyle: UIFont] = [
+//    .largeTitle: UIFont(name: "Poppins-Light", size: 34)!,
+//    .title1: UIFont(name: "Poppins-Light", size: 28)!,
+//    .title2: UIFont(name: "Poppins-Light", size: 22)!,
+//    .title3: UIFont(name: "Poppins-Light", size: 20)!,
+//    .headline: UIFont(name: "Poppins-SemiBold", size: 17)!,
+//    .body: UIFont(name: "Poppins-Light", size: 17)!,
+//    .callout: UIFont(name: "Poppins-Light", size: 16)!,
+//    .subheadline: UIFont(name: "Poppins-Light", size: 15)!,
+//    .footnote: UIFont(name: "Poppins-Light", size: 13)!,
+//    .caption1: UIFont(name: "Poppins-Light", size: 12)!,
+//    .caption2: UIFont(name: "Poppins-Light", size: 11)!
+//]
+//
+//extension UIFont {
+//    class func customFont(forTextStyle style: UIFont.TextStyle) -> UIFont {
+//        let customFont = customFonts[style]!
+//        let metrics = UIFontMetrics(forTextStyle: style)
+//        let scaledFont = metrics.scaledFont(for: customFont)
+//
+//        return scaledFont
+//    }
+//}
+//
+//func fontReplacer() -> {
+//    let styles: [UIFont.TextStyle] = [.largeTitle, .title1, .title2, .title3, .headline, .subheadline, .body, .callout, .footnote, .caption1, .caption2]
+//    for style in styles {
+//        let label = UILabel()
+//        label.adjustsFontForContentSizeCategory = true
+//        label.text = String(describing: style)
+//        label.font = UIFont.customFont(forTextStyle: style)
+//    }
+//}
